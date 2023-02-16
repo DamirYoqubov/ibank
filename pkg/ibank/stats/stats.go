ae111a2a364012428fb70db9872f5f76513e0027
DamirYoqubov <damiryoqubov@gmail.com>
2023-02-16 19:05:48 +0900

    v2

--- pkg/ibank/stats/stats.go
+++ pkg/ibank/stats/stats.go
@@ -1,6 +1,6 @@
 package stats
 
-import "github.com/DamirYoqubov/import/pkg/import/types"
+import "github.com/DamirYoqubov/import/v2/pkg/import/types"
 
 // Avg рассчитывает среднюю сумму платежа
 func Avg(payments []types.Payment) types.Money {
@@ -10,19 +10,21 @@ func Avg(payments []types.Payment) types.Money {
 	count := int32(0)
 
 	for _, payment := range payments {
+		if payment.Status != types.StatusFail{
 		sum += payment.Amount
 		count++
+		}
 	}
 	return sum/types.Money(count)
 }
 
 // TotalInCategory находит сумму покупок в определённой категории.
 func TotalInCategory(payments []types.Payment, category types.Category) types.Money {
-	 
+	
 	sum := types.Money(0)
 
 	for _, payment := range payments {
-		if payment.Category == category {
+		if payment.Category == category && payment.Status != types.StatusFail{
 			sum += payment.Amount
 		}
 	}
