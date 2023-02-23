ae172699fd00bd740e72bbfc1a323ee8c97f24d8
DamirYoqubov <damiryoqubov@gmail.com>
2023-02-23 18:29:23 +0900

    добавлена функция FilterByCategory и тесты

---
+++ pkg/ibank/stats/example_test.go
@@ -0,0 +1,122 @@
+package stats
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/DamirYoqubov/import/v2/pkg/import/types"
+)
+
+func ExampleAvg() {
+	payments := []types.Payment{
+		{
+			Amount: 3,
+		},
+		{
+			Amount: 4,
+		},
+		{
+			Amount: 5,
+		},
+	}
+	avg := Avg(payments)
+	fmt.Println(avg)
+	//Output: 4
+}
+
+func ExampleTotalInCategory()  {
+	category := types.Category("Auto")
+	category2 := types.Category("car")
+	payments := []types.Payment{
+		{
+			Category: category,
+			Amount: 50_00,
+		},
+		{
+			Category: category,
+			Amount: 10_00,
+		},
+		{
+			Category: category2,
+			Amount: 20_00,
+		},
+	}
+	result := TotalInCategory(payments, category)
+
+	fmt.Println(result)
+	//Output: 6000
+}
+
+func TestFilterByCategory_nil(t *testing.T) {
+	var payments []types.Payment
+	result := FilterByCategory(payments, "mobile")
+
+	if len(result) !=0 {
+		t.Error("result len != 0")
+	}
+}
+
+func TestFilterByCategory_empty(t *testing.T) {
+	payments := []types.Payment{}
+	result := FilterByCategory(payments, "mobile")
+
+	if len(result) != 0 {
+		t.Error("result len != 0")
+	}
+}
+
+func TestFIlterByCategory_notFound(t *testing.T) {
+	payments := []types.Payment{
+		{ID: 1, Category: "auto"},
+		{ID: 2, Category: "food"},
+		{ID: 3, Category: "auto"},
+		{ID: 4, Category: "auto"},
+		{ID: 5, Category: "fun"},
+	}
+	result := FilterByCategory(payments, "mobile")
+
+	if len(result) != 0 {
+		t.Error("result len != 0")
+	}
+}
+
+func TestFilterByCategory_foundOne(t *testing.T) {
+	payments := []types.Payment{
+		{ID: 1, Category: "auto"},
+		{ID: 2, Category: "food"},
+		{ID: 3, Category: "auto"},
+		{ID: 4, Category: "auto"},
+		{ID: 5, Category: "fun"},
+	}
+	expected := []types.Payment{
+		{ID: 2, Category: "food"},
+	}
+
+	result := FilterByCategory(payments, "food")
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Error("invalid result")
+	}
+}
+
+func TestFilterByCategory_foundMultiple(t *testing.T) {
+	payments := []types.Payment{
+		{ID: 1, Category: "auto"},
+		{ID: 2, Category: "food"},
+		{ID: 3, Category: "auto"},
+		{ID: 4, Category: "auto"},
+		{ID: 5, Category: "fun"},
+	}
+	expected := []types.Payment{
+		{ID: 1, Category: "auto"},
+		{ID: 3, Category: "auto"},
+		{ID: 4, Category: "auto"},
+	}
+
+	result := FilterByCategory(payments, "auto")
+
+	if !reflect.DeepEqual(expected, result) {
+		t.Error("invalid result")
+	}
+}
